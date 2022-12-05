af06307e9240a083ee0584e303e41fa97b4ee8cc
Sam Mitchell <jfetkotto@gmail.com>
2022-12-05 19:57:50 +0000

    Happy Hacking edit

--- keyboards/lily58/keymaps/jfetkotto/keymap.c
+++ keyboards/lily58/keymaps/jfetkotto/keymap.c
@@ -10,47 +10,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         /* QWERTY
         *,-----------------------------------------.                    ,-----------------------------------------.
-        *| `~   |   1! |   2@ |   3# |   4$ |   5% |                    |   6^ |   7& |   8* |   9( |   0) |  =+  |
+        *| ESC  |   1! |   2@ |   3# |   4$ |   5% |                    |   6^ |   7& |   8* |   9( |   0) |  =+  |
         *|------+------+------+------+------+------|                    |------+------+------+------+------+------|
         *| Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -_  |
         *|------+------+------+------+------+------|                    |------+------+------+------+------+------|
-        *| Esc  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;: |  '"  |
+        *|LCtrl |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;: |  '"  |
         *|------+------+------+------+------+------|   [{  |    |   }]  |------+------+------+------+------+------|
         *|LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,< |   .> |   /? |  |   |
         *`-----------------------------------------/       /     \      \-----------------------------------------'
-        *                 | LCTRL| LAlt |  SYM | /Space  /       \Enter \  | FUNC |BackSP| RGUI |
+        *                 | LGui | SYM  | LAlt | /Space  /       \Enter \  |BackSp| FUNC | RGUI |
         *                 |      |      |      |/       /         \      \ |      |      |      |
         */
 
         [_QWERTY] = LAYOUT(
-        KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+        KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_BSLS),
-                               KC_LCTRL,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
+        KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+LSFT_T(KC_GRV),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_BSLS),
+                               KC_LGUI,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
         ),
 
         /* SYM
         *,-----------------------------------------.                    ,-----------------------------------------.
-        *|      |      |      |      |      |      |                    |      |      |      |      |      |      |
+        *| ESC  |   1! |   2@ |   3# |   4$ |   5% |                    |   6^ |   7& |   8* |   9( |   0) |  =+  |
         *|------+------+------+------+------+------|                    |------+------+------+------+------+------|
         *|      |      |      |      |      |      |                    |      |      |      |      |      |      |
         *|------+------+------+------+------+------|                    |------+------+------+------+------+------|
         *|  `~  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |  \|  |  ;:  |  '"  |
-        *|------+------+------+------+------+------|    (  |    |  )    |------+------+------+------+------+------|
+        *|------+------+------+------+------+------|   [{  |    | }]    |------+------+------+------+------+------|
         *|      |      |      |      |      |      |-------|    |-------|  -_  |  =+  |  ,<  |  .>  |  /?  |
         *`-----------------------------------------/       /     \      \-----------------------------------------'
-        *                | LCTRL| LAlt |  SYM | /Space  /       \Enter \  | FUNC |BackSP| RGUI |
-        *                |      |      |      |/       /         \      \ |      |      |      |
-        *                `----------------------------'           '------''--------------------'
+        *                 | LGui | SYM  | LAlt | /Space  /       \Enter \  |BackSp| FUNC | RGUI |
+        *                 |      |      |      |/       /         \      \ |      |      |      |
         */
 
         [_SYMB] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_GRV,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, KC_TILD,
-        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
-                               KC_LCTRL,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
+ LSFT_T(KC_GRV), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RSFT_T(KC_BSLS),
+                               KC_LGUI,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
         ),
 
         /* FUNC
@@ -63,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         *|------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
         *|LShift|      |      |      |      |      |-------|    |-------|      |      |      |      |      |RShift|
         *`-----------------------------------------/       /     \      \-----------------------------------------'
-        *                | LCTRL| LAlt |  SYM | /Space  /       \Enter \  | FUNC |BackSP| RGUI |
+        *                | LGui | SYM  | LAlt | /Space  /       \Enter \  |BackSp| FUNC | RGUI |
         *                |      |      |      |/       /         \      \ |      |      |      |
         *                `----------------------------'           '------''--------------------'
         */
@@ -72,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_LBRC, KC_RBRC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
-                               KC_LCTRL,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
+ LSFT_T(KC_GRV), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RSFT_T(KC_BSLS),
+                               KC_LGUI,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
         ),
 };
 
@@ -104,10 +103,11 @@ const char *read_mode_icon(bool swap);
 bool oled_task_user(void) {
         if (is_keyboard_master()) {
                 // If you want to change the display of OLED, you need to change here
-                oled_write_ln(read_layer_state(), false);
+                oled_write(read_logo(), false);
+                //oled_write_ln(read_layer_state(), false);
                 //oled_write_ln(read_keylog(), false);
                 //oled_write_ln(read_keylogs(), false);
-                oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+                //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
                 //oled_write_ln(read_timelog(), false);
         } else {
                 oled_write(read_logo(), false);
