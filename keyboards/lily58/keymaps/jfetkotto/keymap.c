87ddb7f659731dcb2e7e4ba2bf95c3d8bfb1826f
Sam Mitchell <jfetkotto@gmail.com>
2023-04-16 09:12:20 +0100

    MacOS

--- keyboards/lily58/keymaps/jfetkotto/keymap.c
+++ keyboards/lily58/keymaps/jfetkotto/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_BSLS),
-                               KC_LCTRL,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
+                               KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
         ),
 
         /* SYM
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_GRV,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, KC_TILD,
         KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RSFT_T(KC_BSLS),
-                               KC_LCTRL,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
+                               KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
         ),
 
         /* FUNC
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
         KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_LBRC, KC_RBRC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RSFT_T(KC_BSLS),
-                               KC_LCTRL,MO(_SYMB), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
+                               KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_FUNC), KC_RGUI
         ),
 };
 
